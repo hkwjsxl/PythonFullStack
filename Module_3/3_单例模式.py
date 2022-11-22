f41253edbfeac8fadfa6310fc8f558ddd205b523
hkw <hkwJsxl@gmail.com>
2022-11-22 22:57:17 +0800

    Edit

--- Module_3/3_单例模式.py
+++ Module_3/3_单例模式.py
@@ -71,3 +71,34 @@ obj1 = Singleton()
 obj2 = Singleton()
 print(obj1, obj2)
 """
+
+"""
+from threading import RLock
+
+
+class Singleton:
+    instance = None
+    lock = RLock()
+
+    def __init__(self, name):
+        self.name = name
+
+    def __new__(cls, *args, **kwargs):
+        if cls.instance:
+            return cls.instance
+
+        with cls.lock:
+            if hasattr(cls, 'instance'):
+                return getattr(cls, 'instance')
+            cls.instance = object.__new__(cls)
+        return cls.instance
+
+
+s = Singleton('alex')
+print(id(s))
+s = Singleton('egon')
+print(id(s))
+
+"""
+
+
